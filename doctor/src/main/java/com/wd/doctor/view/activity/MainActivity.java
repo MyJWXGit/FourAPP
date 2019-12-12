2f416e67be6ba29798359745d7520485fff833ea
jinwanzhen <3071069630@qq.com>
2019-12-12 21:16:39 +0800

    //登录

--- doctor/src/main/java/com/wd/doctor/view/activity/MainActivity.java
+++ doctor/src/main/java/com/wd/doctor/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.wd.doctor;
+package com.wd.doctor.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.wd.doctor.R;
 
 public class MainActivity extends AppCompatActivity {
 
