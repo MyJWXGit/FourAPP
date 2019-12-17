692df4d95daf7984a45effc9eac23b15053570eb
ghk1016 <915230428@qq.com>
2019-12-17 14:31:22 +0800

    video2

--- video/src/main/java/com/wd/video/adapter/Video_QueryAdapter.java
+++ video/src/main/java/com/wd/video/adapter/Video_QueryAdapter.java
@@ -1,40 +1,63 @@
 package com.wd.video.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.lmx.library.media.VideoPlayAdapter;
+import com.bumptech.glide.Glide;
+import com.wd.video.R;
+import com.wd.video.bean.Video_QueryBean;
+
+import java.util.List;
 
 /*
  *author:郭昊坤
  *date:2019/12/13
- *function:*/public class Video_QueryAdapter extends VideoPlayAdapter<Video_QueryAdapter.ViewHolder> {
+ *function:*/public class Video_QueryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+     private List<Video_QueryBean.ResultBean> resultBeans;
+     private Context context;
+    private String path = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576477228199&di=6f862283c719e0618c114253de6943c1&imgtype=0&src=http%3A%2F%2F1882.img.pp.sohu.com.cn%2Fimages%2Fblog%2F2011%2F6%2F6%2F21%2F13%2Fu228722099_1311fe6dc51g213.jpg";
+
+    public Video_QueryAdapter(List<Video_QueryBean.ResultBean> resultBeans, Context context) {
+        this.resultBeans = resultBeans;
+        this.context = context;
+    }
+
     @NonNull
     @Override
-    public Video_QueryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(context).inflate(R.layout.item_video, parent, false);
+        return new ViewHolder(inflate);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Video_QueryAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ViewHolder){
+            Glide.with(context).load(path)
+                    .placeholder(android.R.color.white)
+                    .into(((ViewHolder) holder).mTikTokView);
+        }
 
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return resultBeans.size();
     }
+   public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mTikTokView;
 
-    @Override
-    public void onPageSelected(int itemPosition, View itemView) {
-
-    }
-    class ViewHolder extends RecyclerView.ViewHolder{
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            mTikTokView = itemView.findViewById(R.id.thumb);
         }
+
     }
 }
--- video/src/main/java/com/wd/video/api/API.java
+++ video/src/main/java/com/wd/video/api/API.java
@@ -4,8 +4,8 @@ package com.wd.video.api;
  *author:郭昊坤
  *date:2019/12/13
  *function:*/public interface API {
-     //查看视频类目
-        String VIDEO_ENTRY = "health/user/video/v1/findVideoCategoryList";
-        //根据类目id查询视频
-    String VIDEO_QUERY = "user/video/v1/findVideoVoList";
+    //查看视频类目
+    String VIDEO_ENTRY = "health/user/video/v1/findVideoCategoryList";
+    //根据类目id查询视频
+    String VIDEO_QUERY = "health/user/video/v1/findVideoVoList";
 }
