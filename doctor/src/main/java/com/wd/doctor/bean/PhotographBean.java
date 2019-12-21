8300506ef825ba872a22e6ad20ce9c7317cdb727
jinwanzhen <3071069630@qq.com>
2019-12-21 12:04:54 +0800

    //美化版

---
+++ doctor/src/main/java/com/wd/doctor/bean/PhotographBean.java
@@ -0,0 +1,43 @@
+package com.wd.doctor.bean;
+
+/**
+ * date:2019/12/19
+ * author:金豪(Lenovo)
+ * function:
+ */
+public class PhotographBean {
+
+    /**
+     * result : http://172.17.8.100/images/health/doctor/image_pic/2019-12-19/TWRzlk20191219095115.jpg
+     * message : 上传成功
+     * status : 0000
+     */
+
+    private String result;
+    private String message;
+    private String status;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
