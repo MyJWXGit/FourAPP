7362947306338a1ebfcb8ecf79952416fe9f946d
qwe520 <904858766@qq.com>
2020-01-07 14:08:01 +0800

    MM提交

--- circle/src/main/java/com/wd/circle/view/ReleaseCirclesActivity.java
+++ circle/src/main/java/com/wd/circle/view/ReleaseCirclesActivity.java
@@ -16,12 +16,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -97,7 +99,15 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
     Button releaseCircleBtnPublish;
     @BindView(R.id.release_circle_linear_sick_circle)
     LinearLayout releaseCircleLinearSickCircle;
-//    private ShapeLoadingDialog shapeLoadingDialog;
+    @BindView(R.id.button_hbi3)
+    Button buttonHbi3;
+    @BindView(R.id.aaa)
+    TextView aaa;
+    @BindView(R.id.xuanshangedu_linear)
+    LinearLayout xuanshangeduLinear;
+    @BindView(R.id.swit)
+    Switch swit;
+    //    private ShapeLoadingDialog shapeLoadingDialog;
     private int userId;
     private String sessionId;
     private RecyclerView popup_recycler_department;
@@ -127,6 +137,17 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
     protected void initData() {
         //设置在activity启动的时候输入法默认是不开启的
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        //悬赏额度的开关
+        swit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    xuanshangeduLinear.setVisibility(View.VISIBLE);
+                } else {
+                    xuanshangeduLinear.setVisibility(View.GONE);
+                }
+            }
+        });
         //开始时间
         releaseCircleIvStartTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -316,7 +337,7 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
                 map.put("amount", 0);
 
                 //调发布圈子接口
-                mPresenter.onReplease(userId+"", sessionId, map);
+                mPresenter.onReplease(userId + "", sessionId, map);
 //                shapeLoadingDialog.show();
             }
         });
@@ -397,8 +418,8 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
                     popupWindow.dismiss();
                 }
             });
-        }else if (obj instanceof DiseaseBean){
-            DiseaseBean diseaseBean= (DiseaseBean) obj;
+        } else if (obj instanceof DiseaseBean) {
+            DiseaseBean diseaseBean = (DiseaseBean) obj;
             List<DiseaseBean.ResultBean> result = diseaseBean.getResult();
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
             IllnessAdapter illnessAdapter = new IllnessAdapter(result, this);
@@ -412,17 +433,17 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
                     popWindowDisease.dismiss();
                 }
             });
-        }else if (obj instanceof RepleaseCircleBean){
-            RepleaseCircleBean repleaseCircleBean= (RepleaseCircleBean) obj;
+        } else if (obj instanceof RepleaseCircleBean) {
+            RepleaseCircleBean repleaseCircleBean = (RepleaseCircleBean) obj;
             if (repleaseCircleBean.getStatus().equals("0000")) {
                 Toast.makeText(this, repleaseCircleBean.getMessage(), Toast.LENGTH_SHORT).show();
                 sickCircleId = repleaseCircleBean.getResult();
                 Log.i("sickCircleId", "publishSuccess: " + "sickCircleId" + sickCircleId);
-                if (picture != null){
-                    mPresenter.onPicture(userId+"",sessionId, sickCircleId,picture);
-                }else {
+                if (picture != null) {
+                    mPresenter.onPicture(userId + "", sessionId, sickCircleId, picture);
+                } else {
                     //做任务
-                    mPresenter.onDoTask(userId+"",sessionId,1003);
+                    mPresenter.onDoTask(userId + "", sessionId, 1003);
 //                    shapeLoadingDialog.dismiss();
                     finish();
                 }
@@ -430,21 +451,21 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
             } else {
                 Toast.makeText(this, repleaseCircleBean.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        }else if (obj instanceof DoTaskBean){
-            DoTaskBean doTaskBean= (DoTaskBean) obj;
-            if (doTaskBean.getStatus().equals("0000")){
+        } else if (obj instanceof DoTaskBean) {
+            DoTaskBean doTaskBean = (DoTaskBean) obj;
+            if (doTaskBean.getStatus().equals("0000")) {
                 Toast.makeText(this, "每日首发病友圈完成!快去领取奖励吧", Toast.LENGTH_SHORT).show();
-                mPresenter.onPicture(userId+"",sessionId, sickCircleId,picture);
+                mPresenter.onPicture(userId + "", sessionId, sickCircleId, picture);
             }
-        }else if (obj instanceof PictureBean){
-            PictureBean pictureBean= (PictureBean) obj;
+        } else if (obj instanceof PictureBean) {
+            PictureBean pictureBean = (PictureBean) obj;
             if (pictureBean.getStatus().equals("0000")) {
                 Toast.makeText(this, pictureBean.getMessage(), Toast.LENGTH_SHORT).show();
                 //做任务
-                mPresenter.onDoTask(userId+"",sessionId,1003);
+                mPresenter.onDoTask(userId + "", sessionId, 1003);
 //                shapeLoadingDialog.dismiss();
                 finish();
-            }else {
+            } else {
                 Toast.makeText(this, pictureBean.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
@@ -459,7 +480,7 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //判断是不是选中图片了
-        if (requestCode == 1){
+        if (requestCode == 1) {
             if (resultCode == Activity.RESULT_OK) {
                 Uri uri = data.getData();
                 if (uri != null) {
@@ -481,5 +502,12 @@ public class ReleaseCirclesActivity extends BaseActivity<MainPresenter> implemen
                 Toast.makeText(this, "取消相册", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
